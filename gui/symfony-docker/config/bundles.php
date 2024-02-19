f59140f641aa4c98ba685009b2f6fedc8e86d751
pixelexe <rerezheng@gmail.com>
2024-02-19 22:21:04 +0100

    Migration Database Historic + composer compatible with security bundle

--- gui/symfony-docker/config/bundles.php
+++ gui/symfony-docker/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
