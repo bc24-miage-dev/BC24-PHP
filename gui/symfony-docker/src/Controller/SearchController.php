64a933d5e80ffdd37a678259debc34acc183eb77
shkslxiv <kimseonho133@gmail.com>
2024-02-25 09:15:24 +0900

    Search Debut

--- gui/symfony-docker/src/Controller/SearchController.php
+++ gui/symfony-docker/src/Controller/SearchController.php
@@ -2,17 +2,58 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Resource;
+use App\Form\SearchType;
 
+
+
+#[Route('/search')]
 class SearchController extends AbstractController
 {
-    #[Route('/search', name: 'app_search')]
-    public function search(): Response
-    {
+   
+    #[Route('/', name: 'app_search')]
+    public function search(Request $request, ManagerRegistry $doctrine): Response{
+        $form = $this->createForm(SearchType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $id = $data->getId();
+            $resource = $doctrine
+                ->getRepository(Resource::class)
+                ->find($id);
+            if (!$resource) {
+                $this->addFlash('error', 'Resource not found');
+                return $this->redirectToRoute('app_search');
+            }
+            return $this->redirectToRoute('app_search', ['id' => $id]);
+        }
         return $this->render('search/search.html.twig', [
-            'controller_name' => 'SearchController',
+            'form' => $form->createView(),
         ]);
     }
+
+    #[Route('/{id}', name: 'app_search_result')]
+    public function searchResult($id, ManagerRegistry $doctrine): Response{
+        $resource = $doctrine
+            ->getRepository(Resource::class)
+            ->find($id);
+        if (!$resource) {
+            $this->addFlash('error', 'Resource not found');
+            return $this->redirectToRoute('app_search');
+        }
+        return $this->render('search/search_result.html.twig', [
+            'resource' => $resource,
+        ]);
+    }
+
 }
+    
+
+    
+
