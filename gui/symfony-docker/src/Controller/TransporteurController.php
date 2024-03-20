6dbb14e155ccf8d43371f4482876cf9aeb88d765
shkslxiv <kimseonho133@gmail.com>
2024-03-21 02:53:41 +0900

    Transporteur and Distributeur

--- gui/symfony-docker/src/Controller/TransporteurController.php
+++ gui/symfony-docker/src/Controller/TransporteurController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\ResourceOwnerChangerType;
+use App\Entity\Resource;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/pro/transporteur')]
 class TransporteurController extends AbstractController
@@ -14,4 +18,44 @@ class TransporteurController extends AbstractController
     {
         return $this->render('pro/transporteur/index.html.twig');
     }
+
+
+    #[Route('/acquisition', name: 'app_transporteur_acquire')]
+    public function acquisition(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_TRANSPORTEUR');
+
+        $form = $this->createForm(ResourceOwnerChangerType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $id = $data->getId();
+
+            $resource = $doctrine->getRepository(Resource::class)->find($id);
+
+            $resource->setCurrentOwner($this->getUser());
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($resource);
+            $entityManager->flush();
+            $this->addFlash('success', 'La ressource a bien été enregistrée');
+            return $this->redirectToRoute('app_transporteur_acquire');
+        }
+        return $this->render('pro/transporteur/acquire.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/list', name: 'app_transporteur_list')]
+    public function list(ManagerRegistry $doctrine) : Response
+    {
+        $this->denyAccessUnlessGranted( attribute: 'ROLE_TRANSPORTEUR');
+        $repository = $doctrine->getRepository(Resource::class);
+        $resource = $repository->findBy(['currentOwner' => $this->getUser()]);
+        return $this->render('pro/transporteur/list.html.twig',
+            ['resource' => $resource]
+        );
+    }
+
+   
 }
