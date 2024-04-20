7463728ca8a6aae10d957392b5182415d815a9cc
Dan Kleczewski <Dan.Kleczewski1@etu.univ-paris1.fr>
2024-04-20 21:28:56 +0200

    Equarrisseur refacto

--- gui/symfony-docker/src/Controller/EquarrisseurController.php
+++ gui/symfony-docker/src/Controller/EquarrisseurController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\EquarrisseurAnimalAbattageFormType;
 use App\Form\ResourceOwnerChangerType;
+use App\Handlers\EquarrisseurHandler;
 use App\Handlers\ResourceHandler;
 use App\Handlers\ResourcesListHandler;
 use App\Handlers\TransactionHandler;
@@ -22,10 +23,12 @@ class EquarrisseurController extends AbstractController
 {
 
     private TransactionHandler $transactionHandler;
+    private EquarrisseurHandler $equarrisseurHandler;
 
-    public function __construct(TransactionHandler $transactionHandler)
+    public function __construct(TransactionHandler $transactionHandler, EquarrisseurHandler $equarrisseurHandler)
     {
         $this->transactionHandler = $transactionHandler;
+        $this->equarrisseurHandler = $equarrisseurHandler;
     }
 
     #[Route('/', name: 'app_equarrisseur_index')]
@@ -87,11 +90,11 @@ class EquarrisseurController extends AbstractController
 
         $resource = $resourceRepo->findOneBy(['id' => $id]);
 
-        if (!$resource ||
-            $resource->getCurrentOwner()->getWalletAddress() != $this->getUser()->getWalletAddress()){
+        if (!$this->equarrisseurHandler->canHaveAccess($resource, $this->getUser())){
             $this->addFlash('error', 'Ressource introuvable');
-            return $this->redirectToRoute('app_equarrisseur_index');
+            return $this->redirectToRoute('app_equarrisseur_list', ['category' => 'ANIMAL']);
         }
+
         $category = $resource->getResourceName()->getResourceCategory()->getCategory();
             return $this->render('pro/equarrisseur/job.html.twig', [
                 'resource' => $resource,
@@ -100,35 +103,25 @@ class EquarrisseurController extends AbstractController
     }
 
     #[Route('/equarrir/{id}', name: 'app_equarrisseur_equarrir')]
-    public function equarrir(EntityManagerInterface $entityManager,
+    public function equarrir(ResourceHandler $handler,
                              ResourceRepository $resourceRepo,
-                             ResourceNameRepository $resourceNameRepo,
                              Request $request,
-                             $id)
+                             $id) : Response
     {
         $resource = $resourceRepo->findOneBy(['id' => $id]);
-        if (!$resource || $resource->getResourceName()->getResourceCategory()->getCategory() != 'ANIMAL'
-            || $resource->getCurrentOwner()->getWalletAddress() != $this->getUser()->getWalletAddress())
-        {
-            $this->addFlash('error', 'Il y a eu un problème, veuillez contacter un administrateur');
+        if (!$this->equarrisseurHandler->canSlaughter($resource, $this->getUser())) {
+            $this->addFlash('error', 'Une erreur est survenue, veuillez contacter un administrateur');
             return $this->redirectToRoute('app_equarrisseur_list', ['category' => 'ANIMAL']);
         }
 
-        $handler = new ResourceHandler();
-        $newCarcasse = $handler->createChildResource($resource, $this->getUser());
-        $form = $this->createForm(EquarrisseurAnimalAbattageFormType::class, $newCarcasse);
+        $form = $this->createForm(EquarrisseurAnimalAbattageFormType::class,
+            $newCarcasse = $handler->createChildResource($resource, $this->getUser()));
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $rN = $resourceNameRepo->findOneByCategoryAndFamily('CARCASSE', $resource->getResourceName()->getResourceFamilies()[0]->getName());
-            // Since the $resource is an animal, we can assume that it has only one family (vache, porc, etc.)
-            $newCarcasse->setResourceName($rN);
-            $resource->setIsLifeCycleOver(true);
-            $entityManager->persist($resource);
-            $entityManager->persist($newCarcasse);
-            $entityManager->flush();
+            $this->equarrisseurHandler->slaughteringProcess($resource, $newCarcasse);
             $this->addFlash('success', 'L\'animal a bien été abattu, une carcasse a été créée');
-            return $this->redirectToRoute('app_equarrisseur_index');
+            return $this->redirectToRoute('app_equarrisseur_list', ['category' => 'CARCASSE']);
         }
         return $this->render('pro/equarrisseur/equarrir.html.twig', [
             'form' => $form->createView()
@@ -137,43 +130,22 @@ class EquarrisseurController extends AbstractController
     }
 
     #[Route('/decoupe/{id}', name: 'app_equarrisseur_decoupe')]
-    public function decoupe(EntityManagerInterface $entityManager,
-                            Request $request,
+    public function decoupe(Request $request,
                             ResourceRepository $resourceRepo,
-                            ResourceNameRepository $resourceNameRepo,
-                            ResourceHandler $handler,
                             $id) : Response
     {
         $resource = $resourceRepo->findOneBy(['id'=> $id]);
-        if (!$resource || $resource->getResourceName()->getResourceCategory()->getCategory() != 'CARCASSE'
-        || $resource->getCurrentOwner()->getWalletAddress() != $this->getUser()->getWalletAddress())
+        if (!$this->equarrisseurHandler->canSlice($resource, $this->getUser()))
         {
             $this->addFlash('error', 'Il y a eu un problème, veuillez contacter un administrateur');
             return $this->redirectToRoute('app_equarrisseur_list', ['category' => 'CARCASSE']);
         }
 
-        $demiCarcasse = $resourceNameRepo->findOneByCategoryAndFamily('DEMI-CARCASSE', $resource->getResourceName()->getResourceFamilies()[0]->getName());
-        //Same here, we can assume that a carcasse has only one family
-
-        $newHalfCarcasse = $handler->createChildResource($resource, $this->getUser());
-        $newHalfCarcasse->setResourceName($demiCarcasse);
-        $newHalfCarcasse2 = $handler->createChildResource($resource, $this->getUser());
-        $newHalfCarcasse2->setResourceName($demiCarcasse);
-
         //Classic form because two different entities must be processed at once
         if ($request->isMethod('POST')) {
-            $newHalfCarcasse->setId($request->request->get('tag1'));
-            $newHalfCarcasse2->setId($request->request->get('tag2'));
-            $newHalfCarcasse->setWeight($request->request->get('weight1'));
-            $newHalfCarcasse2->setWeight($request->request->get('weight2'));
-            $resource->setIsLifeCycleOver(true);
-
-            $entityManager->persist($newHalfCarcasse);
-            $entityManager->persist($newHalfCarcasse2);
-            $entityManager->flush();
-
+            $this->equarrisseurHandler->slicingProcess($resource, $this->getUser(), $request);
             $this->addFlash('success', 'Cette carcasse a bien été découpée');
-            return $this->redirectToRoute('app_equarrisseur_index');
+            return $this->redirectToRoute('app_equarrisseur_list', ['category' => 'DEMI-CARCASSE']);
         }
         return $this->render('pro/equarrisseur/slice.html.twig');
     }
--- gui/symfony-docker/src/Handlers/EquarrisseurHandler.php
+++ gui/symfony-docker/src/Handlers/EquarrisseurHandler.php
@@ -2,13 +2,70 @@
 
 namespace App\Handlers;
 
-use App\Handlers\ProHandler;
+use App\Entity\Resource;
+use App\Repository\ResourceNameRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class EquarrisseurHandler extends ProHandler
 {
-    public function __construct(EntityManagerInterface $em)
+    private ResourceNameRepository $resourceNameRepo;
+    private ResourceHandler $resourceHandler;
+
+    public function __construct(EntityManagerInterface $em,
+                                ResourceNameRepository $resourceNameRepo,
+                                ResourceHandler $resourceHandler)
     {
         parent::__construct($em);
+        $this->resourceNameRepo = $resourceNameRepo;
+        $this->resourceHandler = $resourceHandler;
+    }
+
+
+    public function slaughteringProcess(Resource $animal, Resource $carcasse) : void
+    {
+        $rN = $this->resourceNameRepo->findOneByCategoryAndFamily('CARCASSE', $animal->getResourceName()->getResourceFamilies()[0]->getName());
+        // Since the $animal is an animal, we can assume that it has only one family (vache, porc, etc.)
+        $carcasse->setResourceName($rN);
+        $animal->setIsLifeCycleOver(true);
+        $this->entityManager->persist($animal);
+        $this->entityManager->persist($carcasse);
+        $this->entityManager->flush();
+    }
+
+    public function canSlaughter(?Resource $resource, UserInterface $user): bool
+    {
+        return parent::canHaveAccess($resource, $user) &&
+            $resource->getResourceName()->getResourceCategory()->getCategory() == 'ANIMAL';
+    }
+
+    public function slicingProcess(Resource $carcasse,
+                                   UserInterface $user,
+                                   Request $request) : void
+    {
+        $rN = $this->resourceNameRepo->findOneByCategoryAndFamily('DEMI-CARCASSE',
+            $carcasse->getResourceName()->getResourceFamilies()[0]->getName());
+        // Same here, since the $carcasse is a carcasse, we can assume that it has only one family (vache, porc, etc.)
+        $firstHalf = $this->resourceHandler->createChildResource($carcasse, $user);
+        $firstHalf->setResourceName($rN);
+        $secondHalf = $this->resourceHandler->createChildResource($carcasse, $user);
+        $secondHalf->setResourceName($rN);
+
+        $firstHalf->setId($request->request->get('tag1'));
+        $secondHalf->setId($request->request->get('tag2'));
+        $firstHalf->setWeight($request->request->get('weight1'));
+        $secondHalf->setWeight($request->request->get('weight2'));
+        $carcasse->setIsLifeCycleOver(true);
+
+        $this->entityManager->persist($firstHalf);
+        $this->entityManager->persist($secondHalf);
+        $this->entityManager->flush();
+    }
+
+    public function canSlice(?Resource $resource, UserInterface $user): bool
+    {
+        return parent::canHaveAccess($resource, $user) &&
+            $resource->getResourceName()->getResourceCategory()->getCategory() == 'CARCASSE';
     }
 }
